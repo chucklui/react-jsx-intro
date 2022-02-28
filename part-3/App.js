91a62669dff8cbf51d91f4190b236ec81f1b088c
Chuck Lui <luialex0@gmail.com>
2022-02-28 12:31:43 -0800

    finished part 3

---
+++ part-3/App.js
@@ -0,0 +1,11 @@
+'use strict';
+
+function App() {
+  return (
+    <div> 
+      <Person name="Elonnnnnnnnnnnnnn" age={55} hobbies={['crypto', 'tweets', 'Mars']}/> 
+      <Person name="Chuck" age={17} hobbies={['sleep', 'eat', 'space-out']}/>
+      <Person name="Brendan" age={36} hobbies={['games', 'eating', 'wood-working']}/>
+    </div>
+  )
+}
